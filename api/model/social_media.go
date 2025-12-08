cdd8f02e6cf501e416fea02f9f2dfa8d98edf492
Lasa Marndi <lasamarndi199@gmail.com>
2025-12-08 21:16:59 +0530

    Update the changes

--- api/model/social_media.go
+++ api/model/social_media.go
@@ -7,7 +7,7 @@ type SocialLink struct {
 	Name        string     `json:"name" gorm:"size:256"`
 	Url         string     `json:"url" gorm:"size:256"`
 	Type        string     `json:"link" gorm:"size:256"`
-	UserId      uint64     `json:"user_id" gorm:"not null;size:256" `
+	UserId      uint64     `json:"user_id" gorm:"not null;size:256"`
 	MicroSiteId uint64     `json:"micro_site_id"`
 	CreatedAt   *time.Time `json:"-"`
 	UpdatedAt   *time.Time `json:"-"`
