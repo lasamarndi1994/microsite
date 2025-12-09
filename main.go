e8587c649fd155c8a29c7cb4f440918aeb6f1882
lasamarndi1994 <lasamarndi1994@gmail.com>
2025-12-09 13:15:35 +0530

    Update the schedular

--- main.go
+++ main.go
@@ -16,10 +16,8 @@ func main() {
 	database.InitDB(cfg)       // init database
 
 	defer database.CloseDB()
-
 	// Start Scheduler
 	// scheduler.StartCron()
-
 	// Listen and Server in 0.0.0.0:8080
 	router.Run(":8080")
 }
