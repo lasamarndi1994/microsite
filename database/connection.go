7600df616f08dbe6ed8f160ec02fcb8487433535
Lasa Marndi <lasamarndi199@gmail.com>
2025-12-13 13:07:31 +0530

    Update the lead response

--- database/connection.go
+++ database/connection.go
@@ -72,10 +72,11 @@ func CloseDB() {
 func migration(db *gorm.DB) error {
 	// db.AutoMigrate(model.User{})
 	// db.AutoMigrate(model.Otp{})
-	db.AutoMigrate(model.MicroSite{})
+	// db.AutoMigrate(model.MicroSite{})
 	// db.AutoMigrate(model.Service{})
 	// db.AutoMigrate(model.SocialLink{})
-	// db.AutoMigrate(model.Lead{})
+	db.AutoMigrate(model.Lead{})
+	db.AutoMigrate(model.LeadExternalLog{})
 	// db.AutoMigrate(model.Admin{})
 
 	return nil
