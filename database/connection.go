4c5706b1fc1a817676b0e87607e88aa067ab6522
lasaaliceblue <lasa@aliceblueindia.com>
2025-12-15 10:20:15 +0530

    Update the changes

--- database/connection.go
+++ database/connection.go
@@ -3,6 +3,7 @@ package database
 import (
 	"fmt"
 	"log"
+	"micro-site/api/model"
 	"micro-site/config"
 
 	_ "github.com/go-sql-driver/mysql" // MySQL driver
@@ -76,7 +77,7 @@ func migration(db *gorm.DB) error {
 	// db.AutoMigrate(model.SocialLink{})
 	// db.AutoMigrate(model.Lead{})
 	// db.AutoMigrate(model.Admin{})
-	// db.AutoMigrate(model.UserActivity{})
+	db.AutoMigrate(model.UserActivity{})
 
 	return nil
 }
